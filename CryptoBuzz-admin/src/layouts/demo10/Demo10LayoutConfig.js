29a45ce3c786481fe2deb5b48a7575f6f1132d46
Raj <rajpatel01817@gmail.com>
2025-12-11 16:32:35 +0530

    Remove Other layputs

--- CryptoBuzz-admin/src/layouts/demo10/Demo10LayoutConfig.js
+++
@@ -1,7 +0,0 @@
-// Defining the layout configuration specific to demo10 layout
-const Demo10LayoutConfig = {
-  name: 'demo10-layout',
-  // Unique name identifier for this layout
-  options: {}
-};
-export { Demo10LayoutConfig };
