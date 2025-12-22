a29ca9913c5b85e961b8d0f380b9eb47c685b976
pro-dev-pro <devlopmentdevlopment0@gmail.com>
2025-12-22 18:26:12 +0530

    delete auth api at user side

--- CryptoBuzz-be/src/routes/v1/user/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { signup, signIn } from "../../../controllers/user/auth.js";
-
-const router = express.Router();
-
-router.post("/signup", signup);
-router.post("/login", signIn);
-
-export default router;
