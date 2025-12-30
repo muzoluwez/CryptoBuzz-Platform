f3e43ff52714d340b1cbe380001c700cae9ce28f
Raj <devlopmentdevlopment0@gmail.com>
2025-12-30 12:29:44 +0530

    Add Acdemy in fetch category

--- CryptoBuzz-be/src/controllers/admin/category.js
+++ CryptoBuzz-be/src/controllers/admin/category.js
@@ -92,7 +92,7 @@ export const getCategories = async (req, res) => {
 
 export const fetchCategories = async (req, res) => {
   try {
-    const categories = await Category.find({ type: "Courses", status: true })
+    const categories = await Category.find({ type: "Academy", status: true })
       .select("_id name")
       .sort({ createdAt: -1 });
 
