25859d4c5c33d06520e390f050f74c59b3ce3b57
Raj <devlopmentdevlopment0@gmail.com>
2026-01-02 15:58:42 +0530

    fix error

--- CryptoBuzz-be/src/routes/v1/common/index.js
+++ CryptoBuzz-be/src/routes/v1/common/index.js
@@ -13,7 +13,6 @@ import RecordingRoute from "./recording.routes.js";
 import CryptoAnalysisRoute from "./cryptoAnalysis.routes.js";
 import StreamRoute from "./stream.routes.js";
 import healthcheckRoute from "./healthcheck.routes.js";
-import ratingRoute from "./rating.routes.js";
 
 router.use("/auth", AuthRoute);
 router.use("/dashboard", DashboardRoute);
@@ -27,7 +26,6 @@ router.use("/recording", RecordingRoute);
 router.use("/crypto-analysis", CryptoAnalysisRoute);
 router.use("/stream", StreamRoute);
 router.use("/healthCheck", healthcheckRoute);
-router.use("/rating", ratingRoute);
 
 export default router;
 
